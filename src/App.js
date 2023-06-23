d5030c4621ac45d7d1df7cebe3a3ae265cf08ef7
Krishan Kumar <krishankumaar98@gmail.com>
2023-06-23 13:01:59 +0530

    updating nodeapp version

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          hello ......
         </p>
         <a
           className="App-link"
